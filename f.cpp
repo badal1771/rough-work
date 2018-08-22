9bf3d4b8ea375c8d1aa0f34b542bee66d62f150d
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-22 06:57:49 -0400

    Add files via upload

---
+++ f.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a degree,b degree,c degree;
+//enter angles of traingles 
+cout<<"enter the angle of a degree" << "\n";
+cin >> a degree ;
+cout<<"enter the angle of b degree" << "\n";
+cin >> b degree ;
+//calculate the value of area with the help of side of equilateral traingle 
+c degree =(180 degree -a degree -b degree);
+//print the terminal
+cout <<"angle of c degree" << c degree <<"\n";
+return 0;
+}
